0a062ec15f9b87dd6517080cd8e324600cbbbafb
Sara Liang <saralian@andrew.cmu.edu>
2026-06-05 13:55:03 -0400

    remove temp rerouting from testing

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,6 @@ import { redirect } from 'next/navigation'
 export default async function Home() {
   const supabase = await createClient()
   const { data: { user } } = await supabase.auth.getUser()
-  if (user) redirect('/dashboard/settings') // comment out temporarily for testing purposes while dashboard is incomplete
+  if (user) redirect('/dashboard')
   redirect('/login')
 }
